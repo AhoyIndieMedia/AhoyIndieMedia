da385fdb15a743d640ef4a22fb9d81b7c15bc390
Julian Hinsch <jhinsch799@gmail.com>
2022-11-28 16:55:57 -0500

    move things to separate file

---
+++ hours_videos.js
@@ -0,0 +1,167 @@
+const playBlock3 = "https://cdn.jwplayer.com/players/H8BqDxVM-xwwQ9uP0.html";
+const playBlock4 = "https://cdn.jwplayer.com/players/BUJsJkMQ-xwwQ9uP0.html";
+const playBlock5 = "https://cdn.jwplayer.com/players/PdzRKKiA-xwwQ9uP0.html";
+const playBlock6 = "https://cdn.jwplayer.com/players/sEWgfjsC-xwwQ9uP0.html";
+const playBlock7 = "https://cdn.jwplayer.com/players/I2PgVDeT-xwwQ9uP0.html";
+const playBlock8 = "https://cdn.jwplayer.com/players/KFeA3ed2-xwwQ9uP0.html";
+const playBlock1a = "https://cdn.jwplayer.com/players/w4LWEPfF-xwwQ9uP0.html";
+const playBlock2a = "https://cdn.jwplayer.com/players/zjygbzug-xwwQ9uP0.html";
+const playBlock3a = "https://cdn.jwplayer.com/players/H8BqDxVM-xwwQ9uP0.html";
+const playBlock4a = "https://cdn.jwplayer.com/players/BUJsJkMQ-xwwQ9uP0.html";
+const playBlock4c = "https://cdn.jwplayer.com/players/y9AU0Kmk-puZB4zfJ.html";
+const playBlock5a = "https://cdn.jwplayer.com/players/PdzRKKiA-xwwQ9uP0.html";
+const playBlock5b = "https://cdn.jwplayer.com/players/r2CLsAB7-puZB4zfJ.html";
+const playBlock5c = "https://cdn.jwplayer.com/players/SyfFOIKw-xwwQ9uP0.html";
+const playBlock5d = "https://cdn.jwplayer.com/players/HPkC3BmX-puZB4zfJ.html";
+// THE DYNAMIC PLAYBLOCK - THAT SHOULD PLAY ON SATURDAY AND SUNDAY
+const playBlockDynamicA = "https://cdn.jwplayer.com/players/LnzWaP49-puZB4zfJ.html";
+
+//PALBOT TAKEOVER
+const palbotTakeover = "https://cdn.jwplayer.com/players/9ew87ik3-xwwQ9uP0.html";
+
+const hours_videos = {
+    7: {
+        ahoyPushTitle: "playBlock1a",
+        ahoyPlayer: playBlock1a,
+        superAnnounceMobile: "Now Playing <br> Ahoy Morning Classics",
+        superAnnounceDesktop: "Now Playing <br> Ahoy Morning Classics",
+    },
+    8: {
+        ahoyPushTitle: "playBlock1a",
+        ahoyPlayer: playBlock1a,
+        superAnnounceMobile: "Now Playing <br> Ahoy Morning Classics",
+        superAnnounceDesktop: "Now Playing <br> Ahoy Morning Classics"
+    },
+    9: {
+        ahoyPushTitle: "playBlock1a",
+        ahoyPlayer: playBlock1a,
+        superAnnounceMobile: "Now Playing <br> Ahoy Morning Classics",
+        superAnnounceDesktop: "Now Playing <br> Ahoy Morning Classics"
+    },
+    10: {
+        ahoyPushTitle: "playBlock2a",
+        ahoyPlayer: playBlock2a,
+        superAnnounceMobile: "Now Playing <br> Ahoy Daytime Clips",
+        superAnnounceDesktop: "Now Playing <br> Ahoy Daytime Clips"
+    },
+    11: {
+        ahoyPushTitle: "playBlock2a",
+        ahoyPlayer: playBlock2a,
+        superAnnounceMobile: "Now Playing <br>Ahoy Daytime Clips",
+        superAnnounceDesktop: "Now Playing <br>Ahoy Daytime Clips"
+    },
+    12: {
+        ahoyPushTitle: "playBlock2a",
+        ahoyPlayer: playBlock2a,
+        superAnnounceMobile: "Now Playing <br>Ahoy Daytime Clips",
+        superAnnounceDesktop: "Now Playing <br>Ahoy Daytime Clips"
+    },
+    13: {
+        ahoyPushTitle: "playBlock3a",
+        ahoyPlayer: playBlock3a,
+        superAnnounceMobile: "Now Playing <br> Hyper Lofi Music Videos",
+        superAnnounceDesktop: "Now Playing <br> Hyper Lofi Music Videos"
+    },
+    14: {
+        ahoyPushTitle: "playblock3a",
+        ahoyPlayer: playblock3a,
+        superAnnounceMobile: "Now Playing <br> Hyper Lofi Music Videos",
+        superAnnounceDesktop: "Now Playing <br> Hyper Lofi Music Videos"
+    },
+    15: {
+        ahoyPushTitle: "playblock3a",
+        ahoyPlayer: playblock3a,
+        superAnnounceMobile: "Now Playing <br> Hyper Lofi Music Videos",
+        superAnnounceDesktop: "Now Playing <br> Hyper Lofi Music Videos"
+    },
+    16: {
+        ahoyPushTitle: "palbotTakeover",
+        ahoyPlayer: palbotTakeover,
+        superAnnounceMobile: "Now Playing <br> Ahoy Previous Weekly",
+        superAnnounceDesktop: "Now Playing <br> Ahoy Previous Weekly"
+    },
+    17: {
+        ahoyPushTitle: "playBlock4a",
+        ahoyPlayer: playBlock4a,
+        superAnnounceMobile: "Now Playing <br> Ahoy Previous Weekly",
+        superAnnounceDesktop: "Now Playing <br> Ahoy Previous Weekly"
+    },
+    18: {
+        ahoyPushTitle: "playBlock4a",
+        ahoyPlayer: playBlock4a,
+        superAnnounceMobile: "Now Playing <br> Ahoy Previous Weekly",
+        superAnnounceDesktop: "Now Playing <br> Ahoy Previous Weekly"
+    },
+    19: {
+        ahoyPushTitle: "playBlock5c",
+        ahoyPlayer: playBlock5c,
+        superAnnounceMobile: "Now Playing <br> Primetime : The Rob Show ",
+        superAnnounceDesktop: "Now Playing <br> Primetime : The Rob Show "
+    },
+    20: {
+        ahoyPushTitle: "playBlock5c",
+        ahoyPlayer: playBlock5c,
+        superAnnounceMobile: "Now Playing <br> Primetime : The Rob Show ",
+        superAnnounceDesktop: "Now Playing <br> Primetime : The Rob Show "
+    },
+    21: {
+        ahoyPushTitle: "playBlock5c",
+        ahoyPlayer: playBlock5c,
+        superAnnounceMobile: "Now Playing <br> Primetime : The Rob Show",
+        superAnnounceDesktop: "Now Playing <br> Primetime : The Rob Show"
+    },
+    22: {
+        ahoyPushTitle: "playBlock5c",
+        ahoyPlayer: playBlock5c,
+        superAnnounceMobile: "Now Playing <br> Primetime : The Rob Show Episode 7: Jon Peck",
+        superAnnounceDesktop: "Now Playing <br> Primetime : The Rob Show "
+    },
+    23: {
+        ahoyPushTitle: "playBlock5c",
+        ahoyPlayer: playBlock5c,
+        superAnnounceMobile: "Now Playing <br> *NEW* Public Domain Scary for the Sake of Halloween",
+        superAnnounceDesktop: "Now Playing <br> Homemade Lofi Bangers "
+    },
+    24: {
+        ahoyPushTitle: "palbotTakeover",
+        ahoyPlayer: palbotTakeover,
+        superAnnounceMobile: "Now Playing <br> *NEW* Public Domain Scary for the Sake of Halloween",
+        superAnnounceDesktop: "Now Playing <br> Late Night Hodgepodge"
+    },
+    1: {
+        ahoyPushTitle: "palbotTakeover",
+        ahoyPlayer: palbotTakeover,
+        superAnnounceMobile: "Now Playing <br> *NEW* Public Domain Scary for the Sake of Halloween",
+        superAnnounceDesktop: "Now Playing <br> Late Night Hodgepodge"
+    },
+    2: {
+        ahoyPushTitle: "playBlock 6",
+        ahoyPlayer: playBlock6,
+        superAnnounceMobile: "Now Playing <br> *NEW* Public Domain Scary for the Sake of Halloween",
+        superAnnounceDesktop: "Now Playing <br> Late Night Hodgepodge"
+    },
+    3: {
+        ahoyPushTitle: "playBlock 6",
+        ahoyPlayer: playBlock6,
+        superAnnounceMobile: "Now Playing <br> *NEW* Public Domain Scary for the Sake of Halloween",
+        superAnnounceDesktop: "Now Playing <br> Homemade Lofi Bangers "
+    },
+    4: {
+        ahoyPushTitle: "playBlock 7",
+        ahoyPlayer: playBlock7,
+        superAnnounceMobile: "Now Playing <br> Late Night Random Little HomeMade Clips",
+        superAnnounceDesktop: "Now Playing <br> Late Night Random Little HomeMade Clips"
+    },
+    5: {
+        ahoyPushTitle: "playBlock 7",
+        ahoyPlayer: playBlock7,
+        superAnnounceMobile: "Now Playing <br> Late Night Random Little HomeMade Clips",
+        superAnnounceDesktop: "Now Playing <br> Late Night Random Little HomeMade Clips"
+    },
+    6: {
+        ahoyPushTitle: "playBlock 1",
+        ahoyPlayer: playBlock1a,
+        superAnnounceMobile: "Now Playing <br> Late Night Random Little HomeMade Clips",
+        superAnnounceDesktop: "Now Playing <br> Late Night Random Little HomeMade Clips"
+    }
+}
